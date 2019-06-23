602afcd0c669ed4a53db57a06954c3c9a526d161
cerberussg <scott.goyette@rubypythmore.com>
2019-06-23 10:41:11 -0400

    Install django via pip3 with passing test

--- spec/unit/recipes/install_pip_spec.rb
+++ spec/unit/recipes/install_pip_spec.rb
@@ -1,12 +1,12 @@
 #
 # Cookbook:: django
-# Spec:: default
+# Spec:: install_pip
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'django::default' do
+describe 'django::install_pip' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
@@ -17,6 +17,13 @@ describe 'django::default' do
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs the necessary packages' do
+      expect(chef_run).to install_package('python3-pip')
+    end
+    it 'installs django framework' do
+      expect(chef_run).to run_execute('pip3 install django')
     end
   end
 end
