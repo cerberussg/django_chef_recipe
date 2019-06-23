bd6507845f4be5b1459cf6f0787647e49fbc72d3
cerberussg <scott.goyette@rubypythmore.com>
2019-06-22 22:34:33 -0400

    Intial commit ubuntu 18.04 with intial failing test

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,18 +1,5 @@
 # # encoding: utf-8
 
-# Inspec test for recipe django::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe command('django-admin --version') do
+  its(:Stdout) { should match(/1.11.11/) }
 end
